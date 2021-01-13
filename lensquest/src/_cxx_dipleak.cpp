24746d68992233db189df973381d58860ee7316d
Dominic Beck <dmncbck@gmail.com>
2021-01-12 17:50:53 -0800

    fix d distortion

--- lensquest/src/_cxx_dipleak.cpp
+++ lensquest/src/_cxx_dipleak.cpp
@@ -9,11 +9,7 @@ void ldiTE(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, A
 	almG.SetToZero();
 	almC.SetToZero();
 	
-	Alm< xcomplex< double > > almGt(lmax_almG,lmax_almG);
-	Alm< xcomplex< double > > almCt(lmax_almG,lmax_almG);
 	Alm< xcomplex< double > > almZ(lmax_almG,lmax_almG);
-	almGt.SetToZero();
-	almCt.SetToZero();
 	almZ.SetToZero();
 	
 	sharp_cxxjob<double> job;
@@ -33,7 +29,7 @@ void ldiTE(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, A
 	resp1.alloc(lmax_alm1+1);
 	#pragma omp parallel for
 	for (size_t l=2; l<=lmax_alm1; l++) {
-		resp1[l]=sqrt(2.*(l*l+l))*wcl.tt(l);
+		resp1[l]=sqrt((l*l+l))*wcl.tt(l);
 	}
 	
 	almG.Set(lmax_alm1, lmax_alm1);
@@ -50,12 +46,11 @@ void ldiTE(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, A
 	#pragma omp parallel for
 	for (int i=0; i< map1Q.Npix(); i++) {
 		map3[i]= - map2Q[i]*map1U[i] + map2U[i]*map1Q[i];
-		map4[i]= - map2Q[i]*map1Q[i] - map2U[i]*map1U[i];
+		map4[i]= map2Q[i]*map1Q[i] + map2U[i]*map1U[i];
 	}
 	
-	almGt.Set(lmax_almG, lmax_almG);
-	almCt.Set(lmax_almG, lmax_almG);
-	map2alm_spin_iter(job,map3,map4,almGt,almCt,1,3);
+	almG.Set(lmax_almG, lmax_almG);
+	map2alm_spin_iter(job,map3,map4,almG,almZ,1,3);
 	
 	#pragma omp parallel for
 	for (int i=0; i< map1Q.Npix(); i++) {
@@ -63,18 +58,11 @@ void ldiTE(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, A
 		map4[i]= - map2Q[i]*map1U[i] + map2U[i]*map1Q[i];
 	}
 	
-	almG.Set(lmax_almG, lmax_almG);
 	almC.Set(lmax_almG, lmax_almG);
-	map2alm_spin_iter(job,map3,map4,almG,almC,1,3);
+	map2alm_spin_iter(job,map3,map4,almZ,almC,1,3);
 	
-	#pragma omp parallel for
-	for (size_t m=0; m<=lmax_almG; ++m) {
-		for (size_t l=m; l<=lmax_almG; ++l) {
-			tempcomplex=almG(l,m);
-			almG(l,m)=.25*(almGt(l,m)+complex_i*almCt(l,m)+tempcomplex-complex_i*almC(l,m));
-			almC(l,m)=.25*(almGt(l,m)-complex_i*almCt(l,m)+tempcomplex+complex_i*almC(l,m));
-		}
-	}
+	almG.Scale(.5);
+	almC.Scale(.5);
 }
 
 void ldiTB(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, Alm< xcomplex< double > > & almG,  Alm< xcomplex< double > > & almC, PowSpec& wcl, int nside, arr<double> &weight) {
@@ -88,11 +76,7 @@ void ldiTB(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, A
 	almG.SetToZero();
 	almC.SetToZero();
 	
-	Alm< xcomplex< double > > almGt(lmax_almG,lmax_almG);
-	Alm< xcomplex< double > > almCt(lmax_almG,lmax_almG);
 	Alm< xcomplex< double > > almZ(lmax_almG,lmax_almG);
-	almGt.SetToZero();
-	almCt.SetToZero();
 	almZ.SetToZero();
 	
 	sharp_cxxjob<double> job;
@@ -133,20 +117,17 @@ void ldiTB(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, A
 	}
 	
 	
-	almGt.Set(lmax_almG, lmax_almG);
-	almCt.Set(lmax_almG, lmax_almG);
-	map2alm_spin_iter(job,map3,map4,almG,almCt,1,3);
+	almG.Set(lmax_almG, lmax_almG);
+	map2alm_spin_iter(job,map3,map4,almG,almZ,1,3);
 	
 	#pragma omp parallel for
 	for (int i=0; i< map1Q.Npix(); i++) {
-		map3[i]= + map2Q[i]*map1Q[i] + map2U[i]*map1U[i];
-		map4[i]= + map2Q[i]*map1U[i] - map2U[i]*map1Q[i];
+		map3[i]= - map2Q[i]*map1Q[i] - map2U[i]*map1U[i];
+		map4[i]= - map2Q[i]*map1U[i] + map2U[i]*map1Q[i];
 	}
-	
-	
-	almG.Set(lmax_almG, lmax_almG);
+
 	almC.Set(lmax_almG, lmax_almG);
-	map2alm_spin_iter(job,map3,map4,almGt,almC,1,3);	
+	map2alm_spin_iter(job,map3,map4,almZ,almC,1,3);	
 	
 	almG.Scale(.5);
 	almC.Scale(.5);
@@ -163,23 +144,21 @@ void ldiEE(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, A
 	almG.SetToZero();
 	almC.SetToZero();
 	
-	Alm< xcomplex< double > > almGt(lmax_almG,lmax_almG);
-	Alm< xcomplex< double > > almCt(lmax_almG,lmax_almG);
 	Alm< xcomplex< double > > almZ(lmax_almG,lmax_almG);
-	almGt.SetToZero();
-	almCt.SetToZero();
 	almZ.SetToZero();
 	
 	sharp_cxxjob<double> job;
 	job.set_weighted_Healpix_geometry (nside, &weight[0]);
 	
-	Healpix_Map<double> map1Q,map1U,map2Q,map2U,map3,map4;
+	Healpix_Map<double> map1Q,map1U,map2Q,map2U,map3,map4,map5,map6;
 	map1Q.SetNside(nside,RING);
 	map1U.SetNside(nside,RING);
 	map2Q.SetNside(nside,RING);
 	map2U.SetNside(nside,RING);
 	map3.SetNside(nside,RING);
 	map4.SetNside(nside,RING);
+	map5.SetNside(nside,RING);
+	map6.SetNside(nside,RING);
 
 	almZ.Set(lmax_alm2, lmax_alm2);
     alm2map_spin(job,alm2,almZ,map2Q,map2U,2);
@@ -187,7 +166,7 @@ void ldiEE(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, A
 	resp1.alloc(lmax_alm1+1);
 	#pragma omp parallel for
 	for (size_t l=2; l<=lmax_alm1; l++) {
-		resp1[l]=sqrt(2.*(l*l+l))*wcl.tg(l);
+		resp1[l]=sqrt((l*l+l))*wcl.tg(l);
 	}
 	
 	almG.Set(lmax_alm1, lmax_alm1);
@@ -203,31 +182,10 @@ void ldiEE(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, A
 	
 	#pragma omp parallel for
 	for (int i=0; i< map1Q.Npix(); i++) {
-		map3[i]= - map2Q[i]*map1U[i] + map2U[i]*map1Q[i];
+		map3[i]= + map2Q[i]*map1U[i] - map2U[i]*map1Q[i];
 		map4[i]= - map2Q[i]*map1Q[i] - map2U[i]*map1U[i];
-	}
-	
-	almGt.Set(lmax_almG, lmax_almG);
-	almCt.Set(lmax_almG, lmax_almG);
-	map2alm_spin_iter(job,map3,map4,almGt,almCt,1,3);
-	
-	#pragma omp parallel for
-	for (int i=0; i< map1Q.Npix(); i++) {
-		map3[i]= - map2Q[i]*map1Q[i] - map2U[i]*map1U[i];
-		map4[i]= - map2Q[i]*map1U[i] + map2U[i]*map1Q[i];
-	}
-	
-	almG.Set(lmax_almG, lmax_almG);
-	almC.Set(lmax_almG, lmax_almG);
-	map2alm_spin_iter(job,map3,map4,almG,almC,1,3);
-	
-	#pragma omp parallel for
-	for (size_t m=0; m<=lmax_almG; ++m) {
-		for (size_t l=m; l<=lmax_almG; ++l) {
-			tempcomplex=almG(l,m);
-			almG(l,m)=.25*(almGt(l,m)+complex_i*almCt(l,m)+tempcomplex-complex_i*almC(l,m));
-			almC(l,m)=.25*(almGt(l,m)-complex_i*almCt(l,m)+tempcomplex+complex_i*almC(l,m));
-		}
+		map5[i]= - map2Q[i]*map1Q[i] - map2U[i]*map1U[i];
+		map6[i]= + map2Q[i]*map1U[i] - map2U[i]*map1Q[i];
 	}
 	
 	almZ.Set(lmax_alm1, lmax_alm1);
@@ -236,56 +194,35 @@ void ldiEE(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, A
 	resp1.alloc(lmax_alm2+1);
 	#pragma omp parallel for
 	for (size_t l=2; l<=lmax_alm2; l++) {
-		resp1[l]=sqrt(2.*(l*l+l))*wcl.tg(l);
+		resp1[l]=sqrt((l*l+l))*wcl.tg(l);
 	}
 	
-	almGt.Set(lmax_alm2, lmax_alm2);
+	almG.Set(lmax_alm2, lmax_alm2);
 	#pragma omp parallel for
 	for (size_t m=0; m<=lmax_alm2; ++m) {
 		for (size_t l=m; l<=lmax_alm2; ++l) {
-			almGt(l,m)=resp1[l]*alm2(l,m);
+			almG(l,m)=resp1[l]*alm2(l,m);
 		}
 	} 
 	
 	almZ.Set(lmax_alm2, lmax_alm2);
-	alm2map_spin(job,almGt,almZ,map2Q,map2U,1);
+	alm2map_spin(job,almG,almZ,map2Q,map2U,1);
 	
 	#pragma omp parallel for
 	for (int i=0; i< map1Q.Npix(); i++) {
-		map3[i]= - map1Q[i]*map2U[i] + map1U[i]*map2Q[i];
-		map4[i]= - map1Q[i]*map2Q[i] - map1U[i]*map2U[i];
+		map3[i]+=  map1Q[i]*map2U[i] - map1U[i]*map2Q[i];
+		map4[i]+=  map1Q[i]*map2Q[i] + map1U[i]*map2U[i];
+		map5[i]= - map1Q[i]*map2Q[i] - map1U[i]*map2U[i];
+		map6[i]= + map1Q[i]*map2U[i] - map1U[i]*map2Q[i];
 	}
 	
-	almGt.Set(lmax_almG, lmax_almG);
-	almCt.Set(lmax_almG, lmax_almG);
-	map2alm_spin_iter(job,map3,map4,almGt,almCt,1,3);
+	almG.Set(lmax_almG, lmax_almG);
+	almZ.Set(lmax_almG, lmax_almG);
+	map2alm_spin_iter(job,map3,map4,almG,almZ,1,3);
+	map2alm_spin_iter(job,map5,map6,almZ,almC,1,3);
 	
-	#pragma omp parallel for
-	for (size_t m=0; m<=lmax_almG; ++m) {
-		for (size_t l=m; l<=lmax_almG; ++l) {
-			tempcomplex=almG(l,m);
-			almG(l,m)+=.25*(almGt(l,m)+complex_i*almCt(l,m));
-			almC(l,m)+=.25*(almGt(l,m)-complex_i*almCt(l,m));
-		}
-	}
-	
-	#pragma omp parallel for
-	for (int i=0; i< map1Q.Npix(); i++) {
-		map3[i]= - map1Q[i]*map2Q[i] - map1U[i]*map2U[i];
-		map4[i]= - map1Q[i]*map2U[i] + map1U[i]*map2Q[i];
-	}
-	
-	almGt.Set(lmax_almG, lmax_almG);
-	almCt.Set(lmax_almG, lmax_almG);
-	map2alm_spin_iter(job,map3,map4,almGt,almCt,1,3);
-	
-	#pragma omp parallel for
-	for (size_t m=0; m<=lmax_almG; ++m) {
-		for (size_t l=m; l<=lmax_almG; ++l) {
-			almG(l,m)+=.25*(almGt(l,m)-complex_i*almCt(l,m));
-			almC(l,m)+=.25*(almGt(l,m)+complex_i*almCt(l,m));
-		}
-	}	
+	almG.Scale(.5);
+	almC.Scale(.5);
 }
 
 void ldiEB(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, Alm< xcomplex< double > > & almG,  Alm< xcomplex< double > > & almC, PowSpec& wcl, int nside, arr<double> &weight) {
@@ -299,11 +236,7 @@ void ldiEB(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, A
 	almG.SetToZero();
 	almC.SetToZero();
 	
-	Alm< xcomplex< double > > almGt(lmax_almG,lmax_almG);
-	Alm< xcomplex< double > > almCt(lmax_almG,lmax_almG);
 	Alm< xcomplex< double > > almZ(lmax_almG,lmax_almG);
-	almGt.SetToZero();
-	almCt.SetToZero();
 	almZ.SetToZero();
 	
 	sharp_cxxjob<double> job;
@@ -323,7 +256,7 @@ void ldiEB(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, A
 	resp1.alloc(lmax_alm1+1);
 	#pragma omp parallel for
 	for (size_t l=2; l<=lmax_alm1; l++) {
-		resp1[l]=sqrt(2.*(l*l+l))*wcl.tg(l);
+		resp1[l]=sqrt(l*l+l)*wcl.tg(l);
 	}
 	
 	almG.Set(lmax_alm1, lmax_alm1);
@@ -339,32 +272,23 @@ void ldiEB(Alm< xcomplex< double > > & alm1, Alm< xcomplex< double > > & alm2, A
 	
 	#pragma omp parallel for
 	for (int i=0; i< map1Q.Npix(); i++) {
-		map3[i]= - map2Q[i]*map1U[i] + map2U[i]*map1Q[i];
+		map3[i]= + map2Q[i]*map1U[i] - map2U[i]*map1Q[i];
 		map4[i]= map2Q[i]*map1Q[i] + map2U[i]*map1U[i];
 	}
 	
 	
-	almGt.Set(lmax_almG, lmax_almG);
-	almCt.Set(lmax_almG, lmax_almG);
-	map2alm_spin_iter(job,map3,map4,almGt,almCt,1,3);
+	almG.Set(lmax_almG, lmax_almG);
+	map2alm_spin_iter(job,map3,map4,almG,almZ,1,3);
 	
 	#pragma omp parallel for
 	for (int i=0; i< map1Q.Npix(); i++) {
-		map3[i]= map2Q[i]*map1Q[i] + map2U[i]*map1U[i];
-		map4[i]= map2Q[i]*map1U[i] - map2U[i]*map1Q[i];
+		map3[i]= + map2Q[i]*map1Q[i] + map2U[i]*map1U[i];
+		map4[i]= + map2Q[i]*map1U[i] - map2U[i]*map1Q[i];
 	}
-	
-	
-	almG.Set(lmax_almG, lmax_almG);
+
 	almC.Set(lmax_almG, lmax_almG);
-	map2alm_spin_iter(job,map3,map4,almG,almC,1,3);	
+	map2alm_spin_iter(job,map3,map4,almZ,almC,1,3);	
 	
-	#pragma omp parallel for
-	for (size_t m=0; m<=lmax_almG; ++m) {
-		for (size_t l=m; l<=lmax_almG; ++l) {
-			tempcomplex=almG(l,m);
-			almG(l,m)=.25*(almGt(l,m)+complex_i*almCt(l,m)+tempcomplex-complex_i*almC(l,m));
-			almC(l,m)=.25*(almGt(l,m)-complex_i*almCt(l,m)+tempcomplex+complex_i*almC(l,m));
-		}
-	}
+	almG.Scale(.5);
+	almC.Scale(.5);
 }
